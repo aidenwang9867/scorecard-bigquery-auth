6bc3a3b4ff435e608d1b318d9b8bc632e1212321
aidenwang <yidingwang@gmail.com>
2022-06-28 00:01:05 -0700

    temp save 0627-2

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 
 	"github.com/aidenwang9867/scorecard-bigquery-auth/app"
 )
 
-const (
-	PORT = "8080"
-)
-
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	fmt.Printf("Starting HTTP server for scorecard-bigquery-auth on port %s...\n", PORT)
+	fmt.Printf("Starting HTTP server for scorecard-bigquery-auth on port %s...\n", port)
 
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", app.Index)
@@ -24,7 +25,7 @@ func main() {
 	r.HandleFunc("/query/{type}", app.PostResultsHandler).Methods(http.MethodPost)
 	http.Handle("/", r)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", PORT), nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
